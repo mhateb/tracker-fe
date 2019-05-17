65b2bd4aa013a93c287e1f854ad184c7d49ca425
dmitry <maslanovdimasik@gmail.com>
2019-05-17 14:38:28 +0300

    Add chart-pir-react for ratings

--- src/containers/games/test/test-end/test-end.js
+++ src/containers/games/test/test-end/test-end.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactMinimalPieChart from 'react-minimal-pie-chart';
 
 import styles from './test-end.scss';
 
@@ -10,6 +11,22 @@ class TestEnd extends React.PureComponent {
                 <p>Колличество правильных ответов 4 из 15</p>
                 <p>Написать фразу в зависимости от процента правильных ответов</p>
                 <p>Где-то справа надо показать новую диаграмму рейтинга для этого пака</p>
+                <ReactMinimalPieChart
+                    data={[
+                        {
+                            value: 82,
+                            color: '#ffdb17',
+                        },
+                        {
+                            value: 18,
+                            color: 'black'
+                        }
+                    ]}
+                    totalValue={100}
+                    lineWidth={20}
+                    animate
+                    style={{height: '300px'}}
+                />
                 <div>
                     <Link to="/dictionary">Пройти тест снова</Link>
                     <Link to="/dictionary">Вернуться к упражнениям</Link>
